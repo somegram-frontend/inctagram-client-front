0ef39f1d33ffb3876f7ce422054ffd75e8908381
AnatolyNikol <fanka4ectba@mail.ru>
2024-07-25 15:56:09 +0500

    feat: add prettier, stylelint, scss, NavigationLayout. Import fonts. Add settings for layout. Add Nested routes for auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return getLayout(
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
