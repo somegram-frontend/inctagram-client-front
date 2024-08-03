94270c00c3630386b73c1e4e0cc0a65e260b541f
AnatolyNikol <fanka4ectba@mail.ru>
2024-08-03 17:56:20 +0500

    fix: rebase Header and Sidebar from ui-kit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,73 @@
+import s from './header.module.scss'
+
+import { Button, Select } from '@honor-ui/inctagram-ui-kit'
+import {
+  FlagRussia,
+  FlagUnitedKingdom,
+  OutlineBell,
+} from '@honor-ui/inctagram-ui-kit/dist/assets/icons'
+
+type HeaderProps = {
+  isAuth: boolean
+}
+export const Header = ({ isAuth }: HeaderProps) => {
+  const options = [
+    {
+      label: (
+        <div className={s.flagContainer}>
+          <FlagUnitedKingdom /> &nbsp; <span> English</span>
+        </div>
+      ),
+      value: 'value1',
+    },
+    {
+      label: (
+        <div className={s.flagContainer}>
+          <FlagRussia />
+          &nbsp;
+          <span>Russian</span>
+        </div>
+      ),
+      value: 'value2',
+    },
+  ]
+
+  return isAuth ? (
+    <header className={s.header}>
+      <div className={s.logo}>Somegram</div>
+      <div className={s.register}>
+        <div className={s.bell}>
+          <OutlineBell />
+        </div>
+
+        <Select
+          className={s.select}
+          options={options}
+          placeholder={
+            <div className={s.flagContainer}>
+              <FlagUnitedKingdom /> &nbsp; <span> English</span>
+            </div>
+          }
+        />
+      </div>
+    </header>
+  ) : (
+    <header className={s.header}>
+      <div className={s.logo}>Somegram</div>
+      <div className={s.register}>
+        <Select
+          className={s.select}
+          options={options}
+          placeholder={
+            <div className={s.flagContainer}>
+              <FlagUnitedKingdom /> &nbsp; <span> English</span>
+            </div>
+          }
+        />
+        <div className={s.buttons}>
+          <Button variant={'primary'}>Sign up</Button> <Button variant={'primary'}>Sign up</Button>
+        </div>
+      </div>
+    </header>
+  )
+}
