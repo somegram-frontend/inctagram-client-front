be9255093f64a05fe68b3a2b475c4bc3295add3b
AnatolyNikol <fanka4ectba@mail.ru>
2024-08-03 17:22:48 +0500

    feat/#1-auth-API: finish creating auth-api

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,18 +1,16 @@
 import { baseApi } from '@/api/base-api'
+import {
+  loginArgs,
+  registrationArgs,
+  registrationConformationArgs,
+  restorePasswordArgs,
+  restorePasswordConfirmationArgs,
+} from '@/api/auth-api.types'
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
-      google: builder.query<any, void>({
-        query: () => `v1/auth/google`,
-      }),
-      googleCallback: builder.query<any, void>({
-        query: () => `v1/auth/google/callback`,
-      }),
-      github: builder.query<any, void>({
-        query: () => `v1/auth/github`,
-      }),
-      registration: builder.mutation<registrationRequestType, registrationArgs>({
+      registration: builder.mutation<any, registrationArgs>({
         query: body => {
           return {
             url: 'v1/auth/registration',
@@ -21,21 +19,78 @@ export const authApi = baseApi.injectEndpoints({
           }
         },
       }),
+      registrationConformation: builder.mutation<any, registrationConformationArgs>({
+        query: body => {
+          return {
+            url: 'v1/auth/registration-confirmation',
+            method: 'POST',
+            body,
+          }
+        },
+      }),
+      google: builder.query<any, void>({
+        query: () => `v1/auth/google`,
+      }),
+      googleCallback: builder.query<any, void>({
+        query: () => `v1/auth/google/callback`,
+      }),
+      recaptchaSiteKey: builder.query<any, void>({
+        query: () => `v1/auth/recaptcha-site-key`,
+      }),
+      restorePassword: builder.mutation<any, restorePasswordArgs>({
+        query: body => {
+          return {
+            url: 'v1/auth/restore-password',
+            method: 'POST',
+            body,
+          }
+        },
+      }),
+      restorePasswordConfirmation: builder.mutation<any, restorePasswordConfirmationArgs>({
+        query: body => {
+          return {
+            url: 'v1/auth/restore-password-confirmation',
+            method: 'POST',
+            body,
+          }
+        },
+      }),
+      login: builder.mutation<any, loginArgs>({
+        query: body => {
+          return {
+            url: 'v1/auth/login',
+            method: 'POST',
+            body,
+          }
+        },
+      }),
+      logout: builder.mutation<any, void>({
+        query: body => {
+          return {
+            url: 'v1/auth/logout',
+            method: 'POST',
+            body,
+          }
+        },
+      }),
+      refreshToken: builder.mutation<any, void>({
+        query: body => {
+          return {
+            url: 'v1/auth/refresh-token',
+            method: 'POST',
+            body,
+          }
+        },
+      }),
+      github: builder.query<any, void>({
+        query: () => `v1/auth/github`,
+      }),
+      githubCallback: builder.query<any, void>({
+        query: () => `v1/auth/github/callback`,
+      }),
     }
   },
 })
 
 export const { useGoogleQuery, useGoogleCallbackQuery, useGithubQuery, useRegistrationMutation } =
   authApi
-
-type registrationRequestType = {
-  statusCode: number
-  message: string
-}
-
-type registrationArgs = {
-  username: string
-  email: string
-  password: string
-  html: string
-}
