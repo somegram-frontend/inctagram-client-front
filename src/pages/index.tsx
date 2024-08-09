c194abdc96e5a816b1064d8a94e5934667e49812
AnatolyNikol <fanka4ectba@mail.ru>
2024-08-09 13:16:02 +0500

    fix: rebase header and sidebar form ui-kit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import NavigationLayout from '@/components/layout/NavigationLayout'
 /*для импорта стилей с библиотеки*/
 import '@honor-ui/inctagram-ui-kit/css'
-import { Header } from '@/components/header'
 
 export default function Home() {
-  // const data = useGithubQuery()
-  // console.log(data)
-
   return <NavigationLayout isAuth={true}>Main</NavigationLayout>
 }
