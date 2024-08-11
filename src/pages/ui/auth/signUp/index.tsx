00fd682583921c9147cdb01fbaf370f2fe242395
ViktorChizh <victor.chizh@mail.ru>
2024-08-11 20:49:02 +0300

    feat: add FormSignUp component

--- src/pages/ui/auth/signUp/index.tsx
+++ src/pages/ui/auth/signUp/index.tsx
@@ -1,7 +1,11 @@
+import { useRouter } from 'next/router'
 import { FormSignUp, SignUpForm } from './ui/FormSignUp'
 
 const SignUp = () => {
-  const onSubmitSignUp = (formData: SignUpForm) => {}
+  const router = useRouter()
+  const onSubmitSignUp = (formData: SignUpForm) => {
+    router.push('/email-sent')
+  }
 
   return <FormSignUp onSubmit={onSubmitSignUp} />
 }
--- src/pages/ui/auth/signUp/ui/FormSignUp.tsx
+++ src/pages/ui/auth/signUp/ui/FormSignUp.tsx
@@ -1,36 +1,83 @@
-// use 'client'
-import Image from 'next/image'
+'use client'
+
 import { useId } from 'react'
 import { useForm } from 'react-hook-form'
 import Link from 'next/link'
-import { Button, Typography, Cards, Checkbox } from '@honor-ui/inctagram-ui-kit'
+import {
+  GithubSvgrepoCom31 as Github,
+  GoogleSvgrepoCom1 as Google,
+  Button,
+  Typography,
+  Cards,
+} from '@honor-ui/inctagram-ui-kit'
 import '@honor-ui/inctagram-ui-kit/css'
-import github from './GitHub.png'
-import google from './Google.png'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
+import { isValid, z } from 'zod'
 
 import s from './formSignUp.module.scss'
+import { ControlledInput } from '@/components/controlled/ControlledInput'
+import { ControlledCheckbox } from '@/components/controlled/ControlledCheckbox'
 
 type Props = {
   onSubmit: (data: SignUpForm) => void
 }
 
-const loginSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: 'This field has to be filled.' })
-    .email('This is not a valid email.'),
-  password: z
-    .string()
-    .min(4, { message: 'Password must be at least 4 characters' })
-    .max(30, { message: 'The field must not contain more than 30 characters' }),
-  rememberMe: z.boolean().optional(),
-})
+const loginSchema = z
+  .object({
+    username: z
+      .string()
+      .min(1, { message: 'This field has to be filled.' })
+      .min(6, { message: 'Username must be at least 6 characters' })
+      .regex(/^[0-9A-Za-z_-]+$/, {
+        message: 'Username must contain only letters, numbers, underscores, or hyphens.',
+      })
+      .max(30, { message: 'The field must not contain more than 30 characters' }),
+    email: z
+      .string()
+      .min(1, { message: 'This field has to be filled.' })
+      .email('This is not a valid email.'),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters' })
+      .regex(
+        /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~])[0-9A-Za-z!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]+$/,
+        {
+          message:
+            'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.',
+        }
+      )
+      .max(30, { message: 'The field must not contain more than 30 characters' }),
+    confirmPassword: z.string(),
+    isAgree: z.boolean().refine(val => val === true, { message: 'The checkbox must be checked' }),
+  })
+  .refine(
+    values => {
+      return values.password === values.confirmPassword
+    },
+    {
+      message: 'Passwords must match!',
+      path: ['confirmPassword'],
+    }
+  )
 
 export type SignUpForm = z.infer<typeof loginSchema>
+
 export const FormSignUp = ({ onSubmit }: Props) => {
-  const { control, handleSubmit } = useForm<SignUpForm>({ resolver: zodResolver(loginSchema) })
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+    trigger,
+  } = useForm<SignUpForm>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      isAgree: false,
+    },
+  })
   const formId = useId()
 
   return (
@@ -39,15 +86,34 @@ export const FormSignUp = ({ onSubmit }: Props) => {
         Sign Up
       </Typography>
       <div className={s.iconsBlock}>
-        <Image src={google} alt="google" /> {/*TODO change to Google component from library*/}
-        <Image src={github} alt="github" /> {/*TODO change to GitHub component from library*/}
+        <Google />
+        <Github />
       </div>
       <form className={s.form} id={formId} onSubmit={handleSubmit(onSubmit)}>
-        <div className={s.containerInput}>Жду контролируемые инпуты и чекбокс</div>
+        <div className={s.containerInput}>
+          <ControlledInput
+            control={control}
+            label={'Username'}
+            name={'username'}
+            trigger={trigger}
+          />
+          <ControlledInput control={control} label={'Email'} name={'email'} trigger={trigger} />
+          <ControlledInput
+            control={control}
+            label={'Password'}
+            name={'password'}
+            trigger={trigger}
+          />
+          <ControlledInput
+            control={control}
+            label={'Password confirmation'}
+            name={'confirmPassword'}
+            trigger={trigger}
+          />
+        </div>
         <div className={s.containerCheckbox}>
-          {/*<ControlledCheckbox control={control} label={'Remember me'} name={'rememberMe'} />*/}
-          <Checkbox name={'agree'} id="agree" />
-          <Typography as={'label'} htmlFor={'agree'} variant={'small_text'}>
+          <ControlledCheckbox control={control} label={''} name={'isAgree'} trigger={trigger} />
+          <Typography as={'span'} variant={'small_text'}>
             I agree to the{' '}
             <Typography as={Link} href={'/ui/auth/signUp/termsOfService'} variant={'small_link'}>
               Terms of Service
@@ -58,15 +124,20 @@ export const FormSignUp = ({ onSubmit }: Props) => {
             </Typography>
           </Typography>
         </div>
-        <Button form={formId} fullWidth>
+        <div className={s.err}>
+          {errors.isAgree?.message && (
+            <Typography variant={'error_text'}>{errors.isAgree?.message}</Typography>
+          )}
+        </div>
+        <Button form={formId} fullWidth disabled={!isValid}>
           Sign Up
         </Button>
       </form>
       <Typography as={'h2'} className={s.registration} variant={'medium_text14'}>
         Do you have an account?
-      </Typography>
-      <Typography as={Link} className={s.signIn} href={'/ui/auth/signIn'} variant={'regular_link'}>
-        Sign In
+        <Typography as={Link} href={'/ui/auth/signIn'} variant={'regular_link'}>
+          Sign In
+        </Typography>
       </Typography>
     </Cards>
   )
