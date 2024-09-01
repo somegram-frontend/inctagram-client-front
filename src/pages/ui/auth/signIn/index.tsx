a7fcff533242589d4f70e635f0d53c55dd68a396
nakmaykin <nakmaykin@sfxdx.com>
2024-09-01 10:16:01 +0700

    refactor: refactor

--- src/pages/ui/auth/signIn/index.tsx
+++ src/pages/ui/auth/signIn/index.tsx
@@ -1,31 +1,24 @@
-import { FormSignIn, SignInForm } from '@/pages/ui/auth/signIn/formSignIn/FormSignIn'
+import { FormSignIn } from '@/pages/ui/auth/signIn/formSignIn/FormSignIn'
 import { useRouter } from 'next/router'
 import { useLoginMutation } from '@/api/auth-api'
-import { EnumTokens, loginArgs } from '@/api/auth-api.types'
+import { EnumTokens } from '@/api/auth-api.types'
 
 const SignIn = () => {
   const router = useRouter()
   const [signIn, { isError, data, isSuccess, isLoading }] = useLoginMutation()
-  const onSubmitSignIn = ({ email, password }: SignInForm) => {
-    const signInData: loginArgs = {
-      email,
-      password,
-    }
-    signIn(signInData)
-  }
 
   if (isLoading) return <h2>...Loading</h2> // TODO use Preloader
 
   if (isSuccess && data.accessToken) {
     localStorage.setItem(EnumTokens.ACCESS_TOKEN, data.accessToken)
-    router.push(`/`)
+    void router.push(`/`)
   }
 
   return (
     <>
       <FormSignIn
         errorMessage={isError ? 'The email or password are incorrect. Try again please' : ''}
-        onSubmit={onSubmitSignIn}
+        onSubmit={signIn}
       />
     </>
   )
