f2a80e1ef1d3469ff88eda06a9477b2f8604bbd1
ViktorChizh <victor.chizh@mail.ru>
2024-10-06 12:55:19 +0300

    fix: make registration for new backend

--- src/api/auth-api.types.ts
+++ src/api/auth-api.types.ts
@@ -27,79 +27,28 @@ export type LoginArgs = {
   password: string
 }
 
-export type RegistrationResponseSuccess = {
-  statusCode: number
-  message: string
-}
-
-export type RegistrationErrorResponse400 = {
-  error: string
-  details: {
-    email: string
-    username: string
-  }
-}
-
-export type RegistrationErrorResponse422 = {
-  statusCode: number
-  message: string
-  errors: [
-    {
-      property: string
-      constraints: {
-        IsUsername: string
-      }
-    },
-    {
-      property: string
-      constraints: {
-        isEmail: string
-      }
-    },
-    {
-      property: symbol
-      constraints: {
-        length: string
-      }
-    },
-  ]
-}
-
 export type LoginResponse = {
   accessToken: string
 }
 
-export enum EnumTokens {
-  'ACCESS_TOKEN' = 'accessToken',
-  'REFRESH_TOKEN' = 'refreshToken',
-}
-
-export type ConfirmationErrorResponse400 = {
-  error: string
-  message: string
-}
-
-export type ConfirmationErrorResponse422 = {
+export type ConfirmationResponse = {
   statusCode: number
   message: string
-  errors: [
-    {
-      property: string
-      constraints: {
-        IsString: string
-      }
-    },
-  ]
+  error?: string
+  errors?: {
+    property: string
+    constraints: {
+      [key: string]: string
+    }
+  }[]
 }
 
-export type RegistrationResponse = RegistrationResponseSuccess &
-  RegistrationErrorResponse400 &
-  RegistrationErrorResponse422
-
-export type ConfirmationResponse = RegistrationResponseSuccess &
-  ConfirmationErrorResponse400 &
-  ConfirmationErrorResponse422
-
+export type RegistrationResponse = ConfirmationResponse & {
+  details?: {
+    email: string
+    username: string
+  }
+}
 export type MeResponseSuccess = {
   email: string
   userName: string
