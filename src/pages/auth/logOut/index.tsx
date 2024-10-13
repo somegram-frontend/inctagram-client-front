07615e35d3358cb0f36d1be8018f7d20b184bd8c
Anatoly Nikolenko <fanka4ectba@mail.ru>
2024-10-13 22:45:05 +0500

    refactor: pass email in props for LogOut form Sidebar

--- src/pages/auth/logOut/index.tsx
+++ src/pages/auth/logOut/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useLogoutMutation, useMeQuery } from '@/api/auth-api'
+import { useLogoutMutation } from '@/api/auth-api'
 import { Button, LogOut as LogOutIcon } from '@honor-ui/inctagram-ui-kit'
 import s from './logOut.module.scss'
 import { Dialog, DialogClose, DialogContent, DialogTrigger } from '@/components/dialog/Dialog'
@@ -7,8 +7,11 @@ import { Loader } from '@/components/loader/Loader'
 import { RegistrationResponse } from '@/api/auth-api.types'
 import { toast } from 'react-toastify'
 
-const LogOut = () => {
-  const { data } = useMeQuery()
+type Props = {
+  email: string | undefined
+}
+
+const LogOut = ({ email }: Props) => {
   const [logout, { isLoading, isSuccess, isError, error }] = useLogoutMutation()
 
   const onClickHandler = () => {
@@ -48,9 +51,7 @@ const LogOut = () => {
       </DialogTrigger>
       <DialogContent title={'Log Out'}>
         <div className={s.main}>
-          <span className={s.text}>
-            Are you really want to log out of your account {data?.email}?
-          </span>
+          <span className={s.text}>Are you really want to log out of your account {email}?</span>
           <div className={s.buttonContainer}>
             <Button onClick={onClickHandler} variant={'outlined'} className={s.button}>
               Yes
