a83f2555d5d63a45642cefb72431078c7b1c5240
Vladimir Marchenkov <macheteby@gmail.com>
2024-10-15 23:23:20 +0300

    feat: add modal component and add rout for component

---
+++ src/components/dialogAddUserPost/index.tsx
@@ -0,0 +1,86 @@
+import { Button, ImageOutline, PlusSquareOutline } from '@honor-ui/inctagram-ui-kit'
+import { Dialog, DialogContent, DialogTrigger } from '../dialog/Dialog'
+import s from './dialogAddUserPost.module.scss'
+import style from '../../pages/auth/logOut/logOut.module.scss'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import Image from 'next/image'
+import { useAddPhotoForPostMutation } from '@/api/posts-api'
+
+const DialogAddUserPost = () => {
+    const [uploadPhoto, { isLoading }] = useAddPhotoForPostMutation()
+    const [file, setFile] = useState<File | null>(null)
+    const [photo, setPhoto] = useState('')
+    const [error, setError] = useState('')
+
+    const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!photo) {
+            return
+        }
+        if (file) {
+            uploadPhoto({ file })
+        }
+    }
+
+    const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget.files && e.currentTarget.files.length) {
+            const file = e.currentTarget.files[0]
+            if (file.size > 10000000) {
+                setPhoto('')
+                setError('Error! Photo size must be less than 10 MB!')
+            } else if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+                setPhoto('')
+                setError('Error! The format of the uploaded photo must be PNG and JPEG')
+            } else {
+                setFile(file)
+                setError('')
+                const blob = new Blob([file], { type: 'image/jpeg' })
+                const downloadUrl = window.URL.createObjectURL(blob)
+                setPhoto(downloadUrl)
+            }
+        }
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger className={style.triggerButton}>
+                <PlusSquareOutline /> Create
+            </DialogTrigger>
+            <DialogContent title={'Add Photo'}>
+                <div className={s.wrapper}>
+                    {photo ? (
+                        <div className={s.photoContainer}>
+                            <Image src={photo} className={s.photo} alt="" width={190} height={190} />
+                        </div>
+                    ) : (
+                        <div className={s.defaultImageContainer}>
+                            <ImageOutline />
+                        </div>
+                    )}
+                    <form onSubmit={submitHandler}>
+                        <label className={s.inputFile}>
+                            <input type="file" name='photo_upload' accept='image/*' onChange={uploadHandler} />
+                            {photo || <div className={s.btnWrapper}>
+                                <Button as='span' className={s.PhotoBtn}>
+                                    Select from Computer
+                                </Button>
+                                <Button type='submit' variant='outlined'>
+                                    Open Draft
+                                </Button>
+                            </div>
+                            }
+                        </label>
+                        {photo && (
+                            <Button type="submit" className={s.saveAvaBtn}>
+                                Save
+                            </Button>
+                        )}
+                    </form>
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default DialogAddUserPost
+
