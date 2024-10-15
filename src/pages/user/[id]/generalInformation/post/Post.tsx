6c917811faa23a76defc1bb7d1bf3cda6b597640
Anatoly Nikolenko <fanka4ectba@mail.ru>
2024-10-15 11:58:23 +0500

    feat: start add css

--- src/pages/user/[id]/generalInformation/post/Post.tsx
+++ src/pages/user/[id]/generalInformation/post/Post.tsx
@@ -1,3 +1,4 @@
+import { Heart, HeartOutline, MoreHorizontalOutline } from '@honor-ui/inctagram-ui-kit'
 import s from './post.module.scss'
 
 export const Post = () => {
@@ -5,58 +6,64 @@ export const Post = () => {
     <div className={s.postContainer}>
       <img src="" alt="" className={s.postImage} />
       <div className={s.descriptionContainer}>
-        <div className={s.descriptionHeader}>
+        <div className={`${s.descriptionHeader} ${s.wrapper}`}>
           <div className={s.descriptionHeaderProfile}>
             <img src="" alt="" className={s.descriptionAvatarImage} />
-            URLProfile
+            <span className={s.descriptionUserName}>URLProfile</span>
           </div>
-          <button>...</button>
+          <button>
+            <MoreHorizontalOutline className={s.descriptionHeaderButton} />
+          </button>
         </div>
-        <div className={s.descriptionCommentsContainer}>
+        <div className={`${s.descriptionCommentsContainer} ${s.wrapper}`}>
           <div className={s.descriptionCommentContainer}>
             <img src="" alt="" className={s.descriptionAvatarImage} />
             <span className={s.descriptionComment}>
               <b>URLProfiele</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </span>
-            <img src="" alt="" className={s.descriptionCommentIcon} />
-          </div>
-          <div className={s.descriptionCommentContainer}>
-            <img src="" alt="" className={s.descriptionAvatarImage} />
-            <span className={s.descriptionComment}>
-              <b>URLProfiele</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </span>
-            <img src="" alt="" className={s.descriptionCommentIcon} />
-          </div>
-          <div className={s.descriptionCommentContainer}>
-            <img src="" alt="" className={s.descriptionAvatarImage} />
-            <span className={s.descriptionComment}>
-              <b>URLProfiele</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </span>
-            <img src="" alt="" className={s.descriptionCommentIcon} />
-          </div>
-        </div>
-        <div className={s.descriptionLikes}>
-          <div className={s.descriptionLikesIconsContainer}>
-            <div className={s.descriptionLikesIcons}>
-              <img src="" alt="" className={s.descriptionLikesIcon} />
-              <img src="" alt="" className={s.descriptionLikesIcon} />
+            <div className={s.descriptionCommentIconContainer}>
+              <HeartOutline className={s.descriptionCommentIcon} />
             </div>
-            <img src="" alt="" className={s.descriptionLikesIcon} />
           </div>
-          <div className={s.descriptionLikesAvatarsContainer}>
-            <img src="" alt="" className={s.descriptionLikesAvatarImage} />
-            <img src="" alt="" className={s.descriptionLikesAvatarImage} />
-            <img src="" alt="" className={s.descriptionLikesAvatarImage} />
+          <div className={s.descriptionCommentContainer}>
+            <img src="" alt="" className={s.descriptionAvatarImage} />
+            <span className={s.descriptionComment}>
+              <b>URLProfiele</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </span>
+            <div className={s.descriptionCommentIconContainer}>
+              <Heart className={`${s.descriptionCommentIcon} ${s.iconActive}`} />
+            </div>
+          </div>
+          <div className={s.descriptionCommentContainer}>
+            <img src="" alt="" className={s.descriptionAvatarImage} />
+            <span className={s.descriptionComment}>
+              <b>URLProfiele</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </span>
+            <img src="" alt="" className={s.descriptionCommentIcon} />
+          </div>
+        </div>
+        <div className={s.descriptionReactions}>
+          <div className={`${s.descriptionReactionsIconsContainer} ${s.wrapper}`}>
+            <div className={s.descriptionReactionsIcons}>
+              <img src="" alt="" className={s.descriptionReactionsIcon} />
+              <img src="" alt="" className={s.descriptionReactionsIcon} />
+            </div>
+            <img src="" alt="" className={s.descriptionReactionsIcon} />
+          </div>
+          <div className={`${s.descriptionReactionsAvatarsContainer} ${s.wrapper}`}>
+            <img src="" alt="" className={s.descriptionReactionsAvatarImage} />
+            <img src="" alt="" className={s.descriptionReactionsAvatarImage} />
+            <img src="" alt="" className={s.descriptionReactionsAvatarImage} />
             <span>
               2 243 <b>"Like"</b>
             </span>
           </div>
-          <span className={s.date}>July 3, 2021</span>
+          <span className={`${s.date} ${s.wrapper}`}>July 3, 2021</span>
         </div>
-        <div className={s.descriptionFooter}>
+        <div className={`${s.descriptionFooter} ${s.wrapper}`}>
           <span>Add a Comment...</span>
           <button>Publish</button>
         </div>
