aa0286a9117f16c84cc858ac55a505b9965ffb55
k2 <izmail.k2@gmail.com>
2025-01-31 20:39:27 +0300

    fix: translations SSR

--- src/shared/hooks/useTranslation.ts
+++ src/shared/hooks/useTranslation.ts
@@ -1,8 +1,15 @@
 import {useAppSelector} from "@/store";
 import {selectorLanguage} from "@/appRoot/app";
 import {translations} from "@/locales";
+import {useEffect, useState} from "react";
 
 export const useTranslation = () => {
   const language = useAppSelector(selectorLanguage);
-  return translations[language];
-};
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+// Пока язык не определён, возвращаем английский - это нужно для SSR
+  return isMounted ? translations[language] : translations["en"];
+}
