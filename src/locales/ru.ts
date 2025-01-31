b7ee1c60ec08511c256f66c3bc320a4ba07289e2
k2 <izmail.k2@gmail.com>
2025-01-31 22:39:45 +0300

    feat: add type GeneralInformation & Common - add ru & en

--- src/locales/ru.ts
+++ src/locales/ru.ts
@@ -8,18 +8,38 @@ export const ru: Translation = {
     following: 'Подписки',
     followers: 'Подписчики',
     publications: 'Публикации',
-    profileSettings:{
-      title:'Настройка профиля',
-      generalInformation: "Общая информация",
-      devices: "Устройства",
-      accountManagement: "Управление аккаунтом",
-      myPayments: "Мои платежи",
-    }
   },
+  profileSettings: 'Настройка профиля',
+  generalInformation: {
+    title: "Общая информация",
+    username: "Имя пользователя",
+    firstName: "Имя",
+    lastName: "Фамилия",
+    dateOfBirth: "Дата рождения",
+    selectYourCountry: "Выберите страну",
+    selectYourCity: "Выберите город",
+    aboutMe: "Обо мне",
+    addProfilePhoto: "Добавить фото профиля",
+    saveChanges: "Сохранить изменения",
+  },
+
   messenger: "Мессенджер",
   search: "Поиск",
   statistics: "Статистика",
   favorites: "Избранное",
   logout: "Выйти",
+  uploadAvatar: {
+    title: "Загрузить аватар",
+    selectFromComputer: "Выбрать с компьютера",
+  },
+  deletePhoto: {
+    title: 'Удалить фото',
+    confirmationMessage: "Вы уверены, что хотите удалить фото?",
+  },
+  common: {
+    save: 'сохранить',
+    yes: 'да',
+    no: 'нет'
+  }
 };
 
--- src/locales/type.ts
+++ src/locales/type.ts
@@ -1,24 +1,44 @@
-type ProfileSettings = {
+type GeneralInformation = {
   title: string,
-  generalInformation: string,
-  devices: string,
-  accountManagement: string,
-  myPayments: string,
+  username: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  selectYourCountry: string;
+  selectYourCity: string;
+  aboutMe: string;
+  addProfilePhoto: string;
+  saveChanges: string;
+};
+type Profile = {
+  title: string,
+  following: string,
+  followers: string,
+  publications: string,
 }
-type Profile={
-  title:string,
-  following:string,
-  followers:string,
-  publications:string,
-  profileSettings:ProfileSettings,
-}
+type Common = {
+  yes: string;
+  no: string;
+  save: string;
+};
 export type Translation = {
   home: string;
   create: string;
   profile: Profile;
+  profileSettings: string,
+  generalInformation: GeneralInformation,
   messenger: string;
   search: string;
   statistics: string;
   favorites: string;
   logout: string;
+  uploadAvatar: {
+    title: string;
+    selectFromComputer: string;
+  };
+  deletePhoto: {
+    title: string;
+    confirmationMessage: string;
+  };
+  common: Common;
 }
