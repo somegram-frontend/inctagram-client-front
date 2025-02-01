8ce97782d23499ac579dee45b29d93261d59abf2
k2 <izmail.k2@gmail.com>
2025-02-01 21:47:20 +0300

    feat: use useTranslation - change keys type Subscription

--- src/pages/user/[id]/profile/accountManagement/_paymentsAutoRenewal/PaymentsAutoRenewal.tsx
+++ src/pages/user/[id]/profile/accountManagement/_paymentsAutoRenewal/PaymentsAutoRenewal.tsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import s from './PaymentsAutoRenewal.module.scss'
-import { Cards, Checkbox, Typography } from '@honor-ui/inctagram-ui-kit'
-import { safeFormatDate } from '@/shared/utils/formatDate'
+import {Cards, Checkbox, Typography} from '@honor-ui/inctagram-ui-kit'
+import {safeFormatDate} from '@/shared/utils/formatDate'
 import {
   useDisableAutoRenewalMutation,
   useEnableAutoRenewalMutation,
   useGetPaymentsInfoQuery,
 } from '@/api/payments/payments-api'
-import { useDelayedRefetch } from '@/shared/hooks/useDelayedRefetch'
-import { tryCatch } from '@/shared/utils/tryCatch'
-import { Loader } from '@/components/loader'
-import { PaymentsInfoResponse } from '@/api/payments/payments-api.types'
+import {useDelayedRefetch} from '@/shared/hooks/useDelayedRefetch'
+import {tryCatch} from '@/shared/utils/tryCatch'
+import {Loader} from '@/components/loader'
+import {PaymentsInfoResponse} from '@/api/payments/payments-api.types'
+import {useTranslation} from "@/shared/hooks";
 
 type Subscription = {
-  label: 'Expire at' | 'Next payment'
+  label: 'expireAt' | 'nextPayment'
   key: keyof Pick<PaymentsInfoResponse, 'dateOfPayment' | 'endDateOfSubscription'>
 }
 const SUBSCRIPTION: Subscription[] = [
-  { label: 'Expire at', key: 'dateOfPayment' },
-  { label: 'Next payment', key: 'endDateOfSubscription' },
+  {label: 'expireAt', key: 'dateOfPayment'},
+  {label: 'nextPayment', key: 'endDateOfSubscription'},
 ]
 
 const PaymentsAutoRenewal = () => {
-  const [enableAutoRenewal, { isLoading: isEnabling }] = useEnableAutoRenewalMutation()
-  const [disableAutoRenewal, { isLoading: isDisabling }] = useDisableAutoRenewalMutation()
+  const t = useTranslation('accountManagement')
+  const [enableAutoRenewal, {isLoading: isEnabling}] = useEnableAutoRenewalMutation()
+  const [disableAutoRenewal, {isLoading: isDisabling}] = useDisableAutoRenewalMutation()
   const {
     data: paymentsInfo,
     isLoading: isPaymentsInfoLoading,
     refetch,
   } = useGetPaymentsInfoQuery()
-  const { delayedRefetch, isLoading: isRefetching } = useDelayedRefetch(refetch, 3000)
+  const {delayedRefetch, isLoading: isRefetching} = useDelayedRefetch(refetch, 3000)
 
   const toggleAutoRenewal = async () => {
     return tryCatch(async () => {
@@ -45,25 +47,25 @@ const PaymentsAutoRenewal = () => {
   const isDisabled = isLoading || paymentsInfo?.status !== 'Active'
 
   if (isPaymentsInfoLoading) {
-    return <Loader />
+    return <Loader/>
   }
 
   return (
     <>
       {paymentsInfo?.status === 'Active' && (
         <div className={s.paymentsAutoRenewal}>
-          <Typography variant={'h3'}>Current Subscription:</Typography>
+          <Typography variant={'h3'}>{t.subscription}:</Typography>
           <Cards className={s.block}>
-            {SUBSCRIPTION.map(({ label, key }) => (
+            {SUBSCRIPTION.map(({label, key}) => (
               <Typography className={s.blockItem} key={label} variant={'regular_text14'}>
-                <span>{label}</span>
+                <span>{t[label]}</span>
                 {safeFormatDate(paymentsInfo?.[key])}
               </Typography>
             ))}
           </Cards>
 
           <Checkbox
-            label={isRefetching ? 'Updating...' : 'Auto-Renewal'}
+            label={isRefetching ? t.updating + '...' : t.autoRenewal}
             checked={paymentsInfo?.autoRenewal ?? false}
             onCheckedChange={toggleAutoRenewal}
             disabled={isDisabled}
