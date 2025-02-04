1c8275a5cd531e36ad46e1c6beb8592ddf5b0fe7
Vladimir Marchenkov <macheteby@gmail.com>
2025-02-04 20:24:17 +0300

    fix: format

--- src/pages/user/[id]/profile/generalInformation/GeneralInformation.tsx
+++ src/pages/user/[id]/profile/generalInformation/GeneralInformation.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from 'react'
 import s from './GeneralInformation.module.scss'
-import UploadAvatar from "@/pages/user/[id]/profile/uploadProfileAvatar";
-import ProfileForm from "@/pages/user/[id]/profile/profileForm";
-import {useGetProfileQuery, useProfileFillInfoMutation} from "@/api/user/users-api";
-import {ProfileResponse, UserProfile} from "@/api/user/users-api.types";
-import {format} from "date-fns";
-import {Loader} from "@/components/loader";
-import {toast} from "react-toastify";
-import {useRouter} from "next/router";
+import UploadAvatar from '@/pages/user/[id]/profile/uploadProfileAvatar'
+import ProfileForm from '@/pages/user/[id]/profile/profileForm'
+import { useGetProfileQuery, useProfileFillInfoMutation } from '@/api/user/users-api'
+import { ProfileResponse, UserProfile } from '@/api/user/users-api.types'
+import { format } from 'date-fns'
+import { Loader } from '@/components/loader'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/router'
 
 const GeneralInformation = () => {
   const router = useRouter()
 
   const [
     profileFillInfo,
-    {isLoading: isLoadingUpdate, isSuccess: success, error, isError: isErrorUpdate},
+    { isLoading: isLoadingUpdate, isSuccess: success, error, isError: isErrorUpdate },
   ] = useProfileFillInfoMutation()
-  const {data, isLoading, isError: isErrorGet} = useGetProfileQuery()
+  const { data, isLoading, isError: isErrorGet } = useGetProfileQuery()
 
   const onSubmitProfileForm = async (formData: UserProfile) => {
     let formattedDateOfBirth: string | undefined
@@ -28,9 +28,8 @@ const GeneralInformation = () => {
         return
       }
     }
-    await profileFillInfo({...formData, dateOfBirth: formattedDateOfBirth || ''})
+    await profileFillInfo({ ...formData, dateOfBirth: formattedDateOfBirth || '' })
   }
-
 
   if (isErrorGet || isErrorUpdate) {
     const err = error as { data: ProfileResponse }
@@ -46,25 +45,25 @@ const GeneralInformation = () => {
   }
 
   if (success && !toast.isActive('toast-id'))
-    toast.success('Your settings are saved!', {toastId: 'toast-id'})
+    toast.success('Your settings are saved!', { toastId: 'toast-id' })
 
   if (isLoading)
     return (
       <div className={s.loader}>
-        <Loader/>
+        <Loader />
       </div>
     )
 
   return (
     <section className={s.avatarAndForm}>
-      <UploadAvatar/>
+      <UploadAvatar />
       <ProfileForm
         dataValue={data}
         onSubmit={onSubmitProfileForm}
         isLoadingUpdate={isLoadingUpdate}
       />
     </section>
-  );
-};
+  )
+}
 
-export default GeneralInformation;
+export default GeneralInformation
--- src/pages/user/[id]/profile/index.tsx
+++ src/pages/user/[id]/profile/index.tsx
@@ -1,17 +1,16 @@
 'use client'
 import s from './profile.module.scss'
 import Layout from '@/layout'
-import {Tabs} from '@honor-ui/inctagram-ui-kit'
-import {useEffect, useState} from 'react'
+import { Tabs } from '@honor-ui/inctagram-ui-kit'
+import { useEffect, useState } from 'react'
 import AccountManagement from './accountManagement'
 import MyPayments from './MyPayments'
-import {useTranslation} from '@/shared/hooks'
+import { useTranslation } from '@/shared/hooks'
 import GeneralInformation from './generalInformation/GeneralInformation'
 
 const Profile = () => {
   const t = useTranslation()
   const [activeTab, setActiveTab] = useState(t.generalInformation.title)
-
 
   useEffect(() => {
     const savedTab = localStorage.getItem('activeTab')
@@ -28,21 +27,20 @@ const Profile = () => {
     {
       text: t.generalInformation.title,
       value: 'General information',
-      content: <GeneralInformation/>,
+      content: <GeneralInformation />,
     },
-    {text: t.devices.title, value: 'Devices', content: <div>Devices content</div>},
+    { text: t.devices.title, value: 'Devices', content: <div>Devices content</div> },
     {
       text: t.accountManagement.title,
       value: 'Account Management',
-      content: <AccountManagement/>,
+      content: <AccountManagement />,
     },
-    {text: t.myPayments.title, value: 'My payments', content: <MyPayments/>},
+    { text: t.myPayments.title, value: 'My payments', content: <MyPayments /> },
   ]
 
   const handleValueChange = (value: string) => {
     setActiveTab(value)
   }
-
 
   return (
     <Layout>
