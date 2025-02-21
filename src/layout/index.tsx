027c3b36e74ded9dd28b8ef86a14bfc4e1ca3a3d
k2 <izmail.k2@gmail.com>
2025-02-21 16:27:39 +0300

    feat: add socket

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,17 +1,20 @@
-import { Header } from '@/components/header'
-import { Sidebars } from '@/components/sidebar'
-import { NextPage } from 'next'
-import { PropsWithChildren, useEffect } from 'react'
+import {Header} from '@/components/header'
+import {Sidebars} from '@/components/sidebar'
+import {NextPage} from 'next'
+import {PropsWithChildren, useEffect, useState} from 'react'
 import s from './layout.module.scss'
-import { useMeQuery } from '@/api/auth/auth-api'
-import { MeErrorResponse } from '@/api/auth/auth-api.types'
-import { toast } from 'react-toastify'
-import { useRouter } from 'next/router'
+import {useMeQuery} from '@/api/auth/auth-api'
+import {MeErrorResponse} from '@/api/auth/auth-api.types'
+import {toast} from 'react-toastify'
+import {useRouter} from 'next/router'
+import {io, Socket} from "socket.io-client";
 
-const Layout: NextPage<PropsWithChildren> = ({ children }) => {
+
+const Layout: NextPage<PropsWithChildren> = ({children}) => {
   const router = useRouter()
+  const [socket, setSocket] = useState<Socket | null>(null);
 
-  const { data, error } = useMeQuery()
+  const {data, error} = useMeQuery()
 
   const isAuth = !((error as MeErrorResponse)?.status === 401)
 
@@ -25,11 +28,43 @@ const Layout: NextPage<PropsWithChildren> = ({ children }) => {
     }
   }, [error, router])
 
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    console.log(token, 'token')
+    if (!token) return;
+
+    const newSocket = io(`wss://somegram.online/notification`, {
+      transports: ["websocket"],
+      extraHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    newSocket.on("connect", () => {
+      console.log("Подключено к серверу");
+    });
+
+    newSocket.on('notification', (data) => {
+      console.log('New notification received:', data);
+    });
+
+    newSocket.on("connect_error", (error) => {
+      console.error("Ошибка:", error.message);
+    });
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
   return (
     <>
-      <Header isAuth={isAuth} />
+      <Header isAuth={isAuth}/>
       <div className={s.authContent}>
-        {isAuth && <Sidebars isAuth={isAuth} data={data} />}
+        {isAuth && <Sidebars isAuth={isAuth} data={data}/>}
         {children}
       </div>
     </>
