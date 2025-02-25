49dc2bdd2260f001b922b79622253c6009b367cc
k2 <izmail.k2@gmail.com>
2025-02-25 21:06:34 +0300

    feat: add cash

--- src/api/notifications/notifications-api.ts
+++ src/api/notifications/notifications-api.ts
@@ -1,23 +1,24 @@
-import {baseApi} from '@/api/_base/base-api'
-import {ResNotifications} from "@/api/notifications/notifications-api.types";
+import { baseApi } from '@/api/_base/base-api'
+import { ResNotifications } from '@/api/notifications/notifications-api.types'
 
 export const notificationsApi = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
       reedNotifications: builder.mutation<void, { notificationId: string }>({
-        query: ({notificationId}) => {
+        invalidatesTags: ['Notifications'],
+        query: ({ notificationId }) => {
           return {
-            url: '/v1/notifications/read',
+            url: `/v1/notifications/read/${notificationId}`,
             method: 'PUT',
-            params: {notificationId},
           }
         },
       }),
       getNotifications: builder.query<ResNotifications[], void>({
-        query: () => ({url: '/v1/notifications'}),
+        providesTags: ['Notifications'],
+        query: () => ({ url: '/v1/notifications' }),
       }),
     }
   },
 })
 
-export const {useGetNotificationsQuery, useReedNotificationsMutation} = notificationsApi
+export const { useGetNotificationsQuery, useReedNotificationsMutation } = notificationsApi
