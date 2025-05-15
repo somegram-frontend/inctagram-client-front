c698783dc9c1c723b6ffba6e95ca0c06d72a96c7
Nikolay Kevlich <izmail.k2@gmail.com>
2025-05-15 23:25:36 +0300

    fear: add prop

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -3,11 +3,12 @@ import clsx from 'clsx'
 
 type Props = {
   fullHeight?: boolean
+  className?: string
 }
 
-export const Loader = ({ fullHeight }: Props) => {
+export const Loader = ({ fullHeight, className }: Props) => {
   return (
-    <div className={clsx(s.spinnerContainer, fullHeight && s.fullHeight)}>
+    <div className={clsx(s.spinnerContainer, fullHeight && s.fullHeight, className)}>
       <div className={s.loader}></div>
     </div>
   )
