e334b2776b0e55c1f5e31aa5aab341b0f57d9f1f
Nikolay Kevlich <izmail.k2@gmail.com>
2025-05-15 23:25:53 +0300

    feat: add useInfiniteScrollCursor

---
+++ src/pages/posts-following/lib/useInfiniteScroll.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useInfiniteScrollCursor = ({
+  fetchNext,
+  isFetching,
+  hasNext,
+}: {
+  fetchNext: () => void
+  isFetching: boolean
+  hasNext?: boolean
+}) => {
+  const lastElementRef = useRef<HTMLDivElement | null>(null)
+  const observer = useRef<IntersectionObserver | null>(null)
+  const [enabled, setEnabled] = useState(true)
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect()
+
+    observer.current = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setEnabled(true)
+      }
+    })
+
+    if (lastElementRef.current) observer.current.observe(lastElementRef.current)
+  }, [])
+
+  useEffect(() => {
+    if (enabled && !isFetching && hasNext) {
+      fetchNext()
+      setEnabled(false)
+    }
+  }, [enabled, isFetching, hasNext])
+
+  return { lastElementRef }
+}
