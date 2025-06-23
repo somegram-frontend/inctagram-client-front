cdc1f4352ab3996d6c8f66b8764d3704dc8280cd
Stanislav Lobchuk <staslob4uck@gmail.com>
2025-06-23 13:06:21 +0200

    fix: fixed ru locale

--- src/locales/ru.ts
+++ src/locales/ru.ts
@@ -10,7 +10,7 @@ export const ru: Translation = {
     publications: 'Публикации',
     btns: {
       follow: 'Подписаться',
-      unfollow: 'Отриматься',
+      unfollow: 'Отписаться',
       send: 'Отправить сообщение',
     },
   },
